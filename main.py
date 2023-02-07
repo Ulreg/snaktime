5a4c57db29ff78948705442e01bdc6071103d535
Ulreg <ullefry@gmail.com>
2023-02-07 21:55:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ import nltk
 from gtts import gTTS
 
 # Load the audio file
-audio_file = sr.AudioFile('~/PycharmProjects/snaktime/audio/Minoptagelse69.m4a')
+audio_file = sr.AudioFile('/home/ulrich/PycharmProjects/snaktime/audio/Minoptagelse69.flac')
 
 # Transcribe the audio file into text
 r = sr.Recognizer()
 with audio_file as source:
-    audio_text = r.record(source)
+    audio = r.record(source)
 
 # Correct the grammar of the transcribed text
 # Code for grammar correction goes here
@@ -21,13 +21,27 @@ with audio_file as source:
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
+# Tokenize the text
+
+tokens = nltk.word_tokenize(audio.transcription)
+
+# Tag the words with their part of speech
+
+pos_tags = nltk.pos_tag(tokens)
+
+# Correct the grammar using nltk's grammar correction tools
+# (Note: This is a complex task that goes beyond the scope of this code.
+# There are many ways to correct grammar, and the approach you take will depend on your specific use case.)
+
+
 # Parse and correct grammar
 text = nltk.parse.chart.parse(text)
 
 # Generate a text-to-speech audio file
+
 tts = gTTS(text=corrected_text, lang='en')
 tts.save('output.mp3')
 
 # Generate TTS audio file
-tts = gtts.gTTS(text)
-tts.save('path/to/output/file.mp3')
+# tts = gtts.gTTS(text)
+# tts.save('path/to/output/file.mp3')
